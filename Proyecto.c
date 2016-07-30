d6ba780d5ecefa171c8e5f22e6cbda63f1183c09
manugr21 <manugr21@gmail.com>
2016-07-30 15:32:50 -0600

    Analizador v0.3

--- Proyecto.c
+++ Proyecto.c
@@ -80,8 +80,10 @@ struct Journal{
 */
 //Cosas genericas pero importantes
 int     Multiplicador;
+int     convertido;
 char    Normal[400];
 char    Linea_Comparable[400];
+char    Abecedario[28];
 
 //Banderas o indicadores
 int 	TAG;
@@ -138,12 +140,14 @@ void Analizar_Comando(char *linea, char *palabra) {
     char type[2];
     char name[20];
     char path[100];
-    char ruta[100];
+    char ruta_Disco[100];
+    char ruta_Destino[100];
     char fit[2];
     char delete[4];
-    int  add;
+    int  add, dsn;
     char id[5];
     char dfk, dfdm, dfh, dfi;
+    char fs[3];
 
     //Area de creacion de discos
     if(strcasecmp(palabra, "mkdisk") == 0) {
@@ -161,12 +165,12 @@ void Analizar_Comando(char *linea, char *palabra) {
                 strcpy(aux_String, temp);
                 if(aux_String[0] == '\"'){
                     temp++;
-                    strcpy(path, temp);
+                    strcpy(ruta_Disco, temp);
                     temp = strtok(NULL, "\"");
-                    strcat(path, " ");
-                    strcat(path, temp);
+                    strcat(ruta_Disco, " ");
+                    strcat(ruta_Disco, temp);
                 }else{
-                    strcpy(path, temp);
+                    strcpy(ruta_Disco, temp);
                 }
             }else if(strcasecmp(temp,"-name")==0){
             	temp = strtok(NULL, " ");
@@ -181,10 +185,10 @@ void Analizar_Comando(char *linea, char *palabra) {
             temp= strtok(NULL, "::");
         }//Fin del while que obtiene las propiedades
 
-        if((strcasecmp(size,"")!=0)&&(strcasecmp(path,"")!=0)&&(strcasecmp(name,"")!=0)){
+        if((strcasecmp(size,"")!=0)&&(strcasecmp(ruta_Disco,"")!=0)&&(strcasecmp(name,"")!=0)){
 
         	//Crea los directorios si no existen
-            Crear_Directorios_Reales(path);
+            Crear_Directorios_Reales(ruta_Disco);
 
             //Asgino el valor de Kilos o Megas
             if(strcasecmp(unit,"k")==0){
@@ -216,12 +220,12 @@ void Analizar_Comando(char *linea, char *palabra) {
             strcpy(aux_String, temp);
             if(aux_String[0] == '\"'){
                 temp++;
-                strcpy(path, temp);
+                strcpy(ruta_Disco, temp);
                 temp = strtok(NULL, "\"");
-                strcat(path, " ");
-                strcat(path, temp);
+                strcat(ruta_Disco, " ");
+                strcat(ruta_Disco, temp);
             }else{
-                strcpy(path, temp);
+                strcpy(ruta_Disco, temp);
             }
             printf("Eliminaria disco");
         }else if(strcasecmp(temp, "\n") == 0){
@@ -248,13 +252,13 @@ void Analizar_Comando(char *linea, char *palabra) {
                 temp = strtok(NULL, " ");
                 strcpy(aux_String, temp);
                 if(aux_String[0] != '\"'){
-                    strcpy(path, temp);
+                    strcpy(ruta_Disco, temp);
                 }else{
                     temp++;
-                    strcpy(path, temp);
+                    strcpy(ruta_Disco, temp);
                     temp = strtok(NULL, "\"");
-                    strcat(path, " ");
-                    strcat(path, temp);
+                    strcat(ruta_Disco, " ");
+                    strcat(ruta_Disco, temp);
                 }
             }else if(strcasecmp(temp,"+type") == 0){
                 temp = strtok(NULL, " ");
@@ -282,7 +286,7 @@ void Analizar_Comando(char *linea, char *palabra) {
         }
 
         //Validacion de los datos
-        if (((strcasecmp(size,"")!=0)&&(strcasecmp(delete,"")==0))&&(strcasecmp(path,"")!=0)&&(strcasecmp(name,"")!=0)){
+        if (((strcasecmp(size,"")!=0)&&(strcasecmp(delete,"")==0))&&(strcasecmp(ruta_Disco,"")!=0)&&(strcasecmp(name,"")!=0)){
             //Validacion de que el fit este correcto
             if((strcasecmp(fit,"bf") == 0)||(strcasecmp(fit,"ff") == 0)||(strcasecmp(fit,"wf") == 0)||(strcasecmp(fit,"") == 0)){
                 //Validacion de que el type este correcto
@@ -294,7 +298,7 @@ void Analizar_Comando(char *linea, char *palabra) {
             }else{
                 printf("\t>Caracter no reconocido.\n\t>Los tipos de fit disponibles son: BF, FF y WF.\n");
             }
-        }else if((strcasecmp(delete,"")!=0)&&(strcasecmp(path,"")!=0)&&(strcasecmp(name,"")!=0)){
+        }else if((strcasecmp(delete,"")!=0)&&(strcasecmp(ruta_Disco,"")!=0)&&(strcasecmp(name,"")!=0)){
             printf("Eliminaria una particion\n");
         }else{
             printf("\t>No se han escrito todos los datos esenciales, por favor intentelo nuevamente...\n");
@@ -313,12 +317,12 @@ void Analizar_Comando(char *linea, char *palabra) {
                 strcpy(aux_String, temp);
                 if(aux_String[0] == '\"'){
                     temp++;
-                    strcpy(path, temp);
+                    strcpy(ruta_Disco, temp);
                     temp = strtok(NULL, "\"");
-                    strcat(path, " ");
-                    strcat(path, temp);
+                    strcat(ruta_Disco, " ");
+                    strcat(ruta_Disco, temp);
                 }else{
-                    strcpy(path, temp);
+                    strcpy(ruta_Disco, temp);
                 }
             }else if(strcasecmp(temp, "\n") == 0){
 
@@ -342,6 +346,45 @@ void Analizar_Comando(char *linea, char *palabra) {
                 printf("\t>Comando invalido, para desmontar debe ingresar el id asignado a la particion.\n");
         }
         printf("Desmonta una particion.\n");
+    }else if(strcasecmp(palabra, "rep") == 0){
+        char *temp = strtok(linea, " ");
+        temp = strtok(NULL, "::");
+
+        while(temp != NULL){
+            if(strcasecmp(temp,"-name") == 0){
+                temp = strtok(NULL, " ");
+                strcpy(name, temp);
+            }else if(strcasecmp(temp,"-path") == 0){
+                temp = strtok(NULL, " ");
+                strcpy(aux_String, temp);
+                if(aux_String[0] == '\"'){
+                    temp++;
+                    strcpy(ruta_Destino, temp);
+                    temp = strtok(NULL, "\"");
+                    strcat(ruta_Destino, " ");
+                    strcat(ruta_Destino, temp);
+                }else{
+                    strcpy(ruta_Destino, temp);
+                }
+            }else if(strcasecmp(temp,"-id") == 0){
+                temp = strtok(NULL, " ");
+                strcpy(id, temp);
+            }else if(strcasecmp(temp,"+ruta") == 0){
+                temp = strtok(NULL, " ");
+                strcpy(path, temp);
+            }else if(strcasecmp(temp, "\n") == 0){
+
+            }else if(strcasecmp(temp, "\r\n") == 0){
+
+            }else{
+                printf("Comando invalido %s, para desplegar un reporte necesita el nombre, la ruta y el id.\n", temp);
+            }
+            temp = strtok(NULL, "::");
+        }
+        Crear_Directorios_Reales(ruta_Destino);
+        printf("Haria un reporte");
+
+    //Area de lectura de scripts
     }else if (strcasecmp(palabra, "exec") == 0) {
         char *temp = strtok(linea, " ");
         temp = strtok(NULL, "::");
@@ -350,15 +393,15 @@ void Analizar_Comando(char *linea, char *palabra) {
             strcpy(aux_String, temp);
             if(aux_String[0] == '\"'){
                 temp++;
-                strcpy(path, temp);
+                strcpy(ruta_Disco, temp);
                 temp = strtok(NULL, "\"");
-                strcat(path, " ");
-                strcat(path, temp);
+                strcat(ruta_Disco, " ");
+                strcat(ruta_Disco, temp);
             }else{
-                strcpy(path, temp);
+                strcpy(ruta_Disco, temp);
             }
 
-            FILE *f = fopen (path, "r");
+            FILE *f = fopen (ruta_Disco, "r");
 
             if(f != NULL){
                 while(feof(f)==0){
@@ -387,9 +430,129 @@ void Analizar_Comando(char *linea, char *palabra) {
         }else{
                 printf("\t>Para ejecutar un script unicamente debe ingresar la ruta del archivo con el comando -path.\n");
         }
-    }else if (strcasecmp(palabra, "exit") == 0) {
+    }else if(strcasecmp(palabra, "df") == 0){
+    	char *temp = strtok(linea, " ");
+    	temp = strtok(NULL, " ");
+
+    	while(temp != NULL){
+    		if(strcasecmp(temp, "+k") == 0){
+    			dsk = 'k';
+    		}else if(strcasecmp(temp, "+m") == 0){
+    			dsm = 'm';
+    		}else if(strcasecmp(temp, "+h") == 0){
+    			dsh = 'h';
+    		}else if(strcasecmp(temp, "+i") == 0){
+    			dsi = 'i';
+    		}else if(strcasecmp(temp, "\n") == 0){
+
+            }else if(strcasecmp(temp, "\r\n") == 0){
+
+            }else{
+                printf("\t>Comando invalido, para disk free.\n");
+            }
+    		temp = strtok(NULL, " ");
+    	}
+
+    	if((dsk == NULL) || (dsm == NULL) || (dsh == NULL) || (dsi == NULL)){
+            dsk = 'k'
+    	}
+
+    	printf("Hace lo del disk free.\n");
+
+    }else if(strcasecmp(palabra, "du") == 0){
+    	char *temp = strtok(linea, " ");
+    	temp = strtok(NULL, "::");
+
+    	while(temp != NULL){
+    		if(strcasecmp(temp, "+n") == 0){
+    			temp = strtok(NULL, " ");
+    			dsn = (int)strtol(temp, (char **)NULL, 10);
+    		}else if(strcasecmp(temp, "-h") == 0){
+                temp = strtok(NULL, " ");
+    			dsh = 'h';
+    		}else if(strcasecmp(temp, "-path") == 0){
+                temp = strtok(NULL, " ");
+                strcpy(aux_String, temp);
+                if(aux_String[0] == '\"'){
+                    temp++;
+                    strcpy(ruta_Disco, temp);
+                    temp = strtok(NULL, "\"");
+                    strcat(ruta_Disco, " ");
+                    strcat(ruta_Disco, temp);
+                }else{
+                    strcpy(ruta_Disco, temp);
+                }
+    		}else if(strcasecmp(temp, "\n") == 0){
+
+            }else if(strcasecmp(temp, "\r\n") == 0){
+
+            }else{
+                printf("\t>Comando invalido, para disk used.\n");
+            }
+    		temp = strtok(NULL, "::");
+    	}
+    	if((ruta_Disco != "") && (dsh == 'h')){
+            printf("Hace lo del disk used.\n");
+    	}
+
+    }else if (strcasecmp(palabra, "mkfs") == 0) {
+        char *temp = strtok(linea, " ");
+        temp = strtok(NULL, "::");
+
+        while(temp != NULL){
+            if(strcasecmp(temp,"-id") == 0){
+                temp = strtok(NULL, " ");
+                strcpy(id, temp);
+            }else if(strcasecmp(temp,"+type") == 0){
+                temp = strtok(NULL, " ");
+                strcpy(type, temp);
+            }else if(strcasecmp(temp,"+add") == 0){
+                temp = strtok(NULL, " ");
+                add = (int)strtol(temp, (char**)NULL, 10);
+            }else if(strcasecmp(temp,"+unit") == 0){
+                temp = strtok(NULL, " ");
+                strcpy(unit, temp);
+            }else if(strcasecmp(temp,"+fs") == 0){
+                temp = strtok(NULL, " ");
+                strcpy(fs, temp);
+            }else if(strcasecmp(temp, "\n") == 0){
+
+            }else if(strcasecmp(temp, "\r\n") == 0){
+
+            }else{
+                printf("\t>Comando invalido para mkfs.\n");
+            }
+            temp = strtok(NULL, ":");
+        }
+
+        if((strcasecmp(type, "fast") == 0) || (strcasecmp(type, "full") == 0)){
+
+        }else if(strcasecmp(type, "")){
+            strcpy(type, "full");
+        }else{
+            printf("El tipo de formateo no es valido.\n");
+        }
+
+        if(strcasecmp(id, "") == 0){
+            printf("Se necesita el modificador id para realizar formateos.\n");
+        }else{
+            char c = Jarvis.id[2];
+            char ns[3];
+            sprintf(ns, "%c%c", Jarvis.id[3],Jarvis.id[4]);
+            aux_int = 0;
+            while(c != Abecedario[aux_int]){
+                aux_int++;
+            }
+            convertido = (int)strtol(ns, (char **)NULL, 10);
+            strcpy(name, Montador[aux_int][convertido]);
+            strcpy(ruta_Disco, Montador[aux_int][0]);
+            //Formatear(name, ruta_Disco);
+            printf("Formatearia la particion")ñ
+        }
+
+    }else if(strcasecmp(palabra, "exit") == 0) {
         TAG_Script = -7;
-    }else if (strcasecmp(palabra, "clear") == 0) {
+    }else if(strcasecmp(palabra, "clear") == 0) {
         system("clear");
     }else{
         printf("\t>Comando invalido. Por favor intentelo nuevamente...\n");
@@ -416,7 +579,9 @@ int main()
     printf("************************************************************\n");
     printf("*        Bienvenido al sistema de archivos Ext             *\n");
     printf("************************************************************\n");
-    //Preparar();
+    time_t t;
+    srand((unsigned) time(&t));
+    strcpy(Abecedario,"abcdefghijklmnopqrstuvwxyz");
     printf(">>Para apagar el sistema ingrese el comando \"exit\".\n>> Sistema listo, porfavor introduzca un comando...\n");
     Ingresar_Comando();
     printf("Apagando...\n");
